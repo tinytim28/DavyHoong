5ac7819d559be05d3206589cd4a3010639148686
Unknown <jh.tan.2015@sis.smu.edu.sg>
2018-03-07 21:08:17 +0800

    Notification shit
    yessir

---
+++ Savvy0.5/src/java/DAO/NotificationDAO.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import classes.Notification;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import connection.ConnectionManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.Date;
+import java.time.*;
+
+/**
+ *
+ * @author Timothy
+ */
+public class NotificationDAO {
+    
+    private Connection conn;
+    private ResultSet result;
+    private PreparedStatement stmt;
+    
+    public String retrieveTeamNotification(int managerID) {
+        JsonArray jsonArray = new JsonArray();
+        try {
+            conn = ConnectionManager.getConnection();
+            String query = "SELECT * from notification where managerID like '" + managerID + "' and audience = 0";
+            stmt = conn.prepareStatement(query);
+            result = stmt.executeQuery();
+
+            while (result.next()) {
+
+                JsonObject toReturn = new JsonObject();
+
+                toReturn.addProperty("managerID", result.getInt("managerID"));
+                toReturn.addProperty("notificationType", result.getString("notificationType"));
+                toReturn.addProperty("date", result.getDate("date").toString());
+                
+                if (result.getString("notificationType").toLowerCase().equals("roadshow") || result.getString("notificationType").toLowerCase().equals("calling session") || result.getString("notificationType").toLowerCase().equals("meeting")) {       
+                    toReturn.addProperty("start", result.getTime("start").toString());
+                    toReturn.addProperty("end", result.getTime("end").toString());
+                }
+                
+                toReturn.addProperty("end", result.getTime("end").toString());
+                toReturn.addProperty("audience", result.getInt("audience"));
+                jsonArray.add(toReturn);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                result.close();
+                stmt.close();
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return jsonArray.toString();
+    }
+    
+    public String retrieveIndividualNotification(int managerID, int agentID) {
+        JsonArray jsonArray = new JsonArray();
+        try {
+            conn = ConnectionManager.getConnection();
+            String query = "SELECT * from notification where managerID like '" + managerID + "' and audience = '" + agentID + "'";
+            stmt = conn.prepareStatement(query);
+            result = stmt.executeQuery();
+
+            while (result.next()) {
+
+                JsonObject toReturn = new JsonObject();
+
+                toReturn.addProperty("managerID", result.getInt("managerID"));
+                toReturn.addProperty("notificationType", result.getString("notificationType"));
+                toReturn.addProperty("date", result.getDate("date").toString());         
+                toReturn.addProperty("audience", result.getInt("audience"));
+                jsonArray.add(toReturn);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                result.close();
+                stmt.close();
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return jsonArray.toString();
+    }
+}
---
+++ Savvy0.5/src/java/classes/Notification.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.sql.Date;
+import java.sql.Time;
+
+/**
+ *
+ * @author Timothy
+ */
+public class Notification {
+    int managerID;
+    String notificationType;
+    String message;
+    Date date;
+    Time start;
+    Time end;
+    int priority;
+    int audience;  // 0 if meant for team, else individual agentID
+
+    public Notification(int managerID, String notificationType, Date date, Time start, Time end, int priority, int audience, String message) {
+        this.managerID = managerID;
+        this.notificationType = notificationType;
+        this.date = date;
+        this.start = start;
+        this.end = end;
+        this.priority = priority;
+        this.audience = audience;
+        this.message = message;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    public int getManagerID() {
+        return managerID;
+    }
+
+    public void setManagerID(int managerID) {
+        this.managerID = managerID;
+    }
+
+    public String getNotificationType() {
+        return notificationType;
+    }
+
+    public void setNotificationType(String notificationType) {
+        this.notificationType = notificationType;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getStart() {
+        return start;
+    }
+
+    public void setStart(Time start) {
+        this.start = start;
+    }
+
+    public Time getEnd() {
+        return end;
+    }
+
+    public void setEnd(Time end) {
+        this.end = end;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getAudience() {
+        return audience;
+    }
+
+    public void setAudience(int audience) {
+        this.audience = audience;
+    }
+    
+    
+}
